20846dd4a2eda2cffd036f2bf2e0f88fd85517be
Pete Micsunescu <peat.is.nete@gmail.com>
2021-03-18 16:54:09 -0500

    Added SFX. Updated 'turn' text.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 //alert("working");
 
+var clickSound = new Audio("click-sound.mp3");
 var statusBar = document.querySelector(".status-bar");
 var container = document.querySelector(".square");
+var cell = document.querySelectorAll(".grid-item");
+var xMarker = document.querySelector('#x');
+var oMarker = document.querySelector('#o');
+var refreshButton = document.querySelector("button");
+var turnText = document.querySelector(".turn");
+var subBar = document.getElementById(subBar);
+
+
+refreshButton.addEventListener("click", function() {
+    location.reload()
+})
+
+console.log(xMarker);
+ 
+
 var turn = 0;
 var player = 0;
+
 var win = [
         [1,2,3],
         [4,5,6],
@@ -15,33 +32,58 @@ var win = [
 var p0 = [];
 var p1 = [];
 
- 
 
-container.addEventListener("click", function(event){
-    console.log(event);
+function colorChanger (event) {
+     
     let whatIsClicked = event.target;
-
     player = turn%2;
+    
 
     if (player == 0){
-        whatIsClicked.style.backgroundColor = "SteelBlue";
+        
+        clickSound.play();
+        let newX = xMarker.cloneNode(true);
+        newX.classList.remove("no-display");
+        whatIsClicked.appendChild(newX);
+        newX.parentNode.classList.remove("select");
         p0.push(event.target.id);
-        statusBar.style.backgroundColor = "Tomato";
+        turnText.innerHTML = "o";
+         
+         
     }
     if (player == 1){
-        whatIsClicked.style.backgroundColor = "Tomato";
+        
+        clickSound.play();
+        let newO = oMarker.cloneNode(true);
+        newO.classList.remove("no-display", "select");
+        whatIsClicked.appendChild(newO);
+        newO.parentNode.classList.remove("select");
         p1.push(event.target.id);
-        statusBar.style.backgroundColor = "SteelBlue";
+        turnText.innerHTML = "x";
+         
+         
     }
 
 
     turn++;
-     
+    
      
     console.log(player, "player");
     console.log(turn, "turn");
     console.log(event.target);
-})
+
+
+}
+
+console.log(cell);
+
+
+
+cell.forEach( function(e){
+    e.addEventListener("click", colorChanger, {once:true});
+});
+
+//container.addEventListener("click", colorChanger);
 
 console.log(container);
 
